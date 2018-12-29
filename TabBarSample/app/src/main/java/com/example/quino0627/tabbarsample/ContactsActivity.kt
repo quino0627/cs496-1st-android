7c335ee39e4b8ef05d693385fb102afdf1538cbb
송동욱 <quino0627@korea.ac.kr>
2018-12-30 00:49:45 +0900

    added profile Image

--- TabBarSample/app/src/main/java/com/example/quino0627/tabbarsample/ContactsActivity.kt
+++ TabBarSample/app/src/main/java/com/example/quino0627/tabbarsample/ContactsActivity.kt
@@ -1,13 +1,6 @@
 package com.example.quino0627.tabbarsample
 
-import android.Manifest
-import android.app.Fragment
-import android.content.ContentResolver
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.provider.ContactsContract
-import android.support.v4.app.ActivityCompat
-import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
@@ -17,16 +10,8 @@ import android.view.ViewGroup
 import android.widget.LinearLayout
 import kotlinx.android.synthetic.main.fragment_contact.*
 import com.example.quino0627.tabbarsample.MainActivity.Companion.contactsList
-import com.example.quino0627.tabbarsample.R.id.contacts_recycler_view
 
 class ContactsActivity : android.support.v4.app.Fragment(){
-
-    companion object {
-        fun newInstance(): ContactsActivity {
-            val fragment = ContactsActivity()
-            return fragment
-        }
-    }
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -38,35 +23,12 @@ class ContactsActivity : android.support.v4.app.Fragment(){
         val adapter = ContactsAdapter(contactsList!!)
 
         val formanage = LinearLayoutManager(activity, LinearLayout.VERTICAL, false)
-        //contacts_recycler_view?.layoutManager = LinearLayoutManager(this,LinearLayout.VERTICAL, false)
         recyclerView.layoutManager = formanage
         recyclerView.adapter = adapter
         recyclerView.setHasFixedSize(false)
-//        return inflater.inflate(R.layout.fragment_contact, container, false)
         return rootView
 
     }
 
-//    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-//        if (requestCode == REQUEST_CONTACT)setContacts()
-//    }
-//
-//    fun setContacts() {
-//
-//        val contactsList: ArrayList<Contact> = ArrayList()
-//        val cursor = getContentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null)
-//        while (cursor.moveToNext()) {
-//            contactsList.add(
-//                Contact(
-//                    cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)),
-//                    cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
-//                )
-//            )
-//        }
-//        cursor.close()
-//        val adapter = ContactsAdapter(contactsList)
-//        contacts_recycler_view.layoutManager = LinearLayoutManager(MainActivity(),LinearLayout.VERTICAL, false)
-//        contacts_recycler_view.adapter = adapter
-//    }
 
 }
